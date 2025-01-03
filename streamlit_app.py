ce173e2974e0f75e6af2fb338de93555871afd39
fengp <30399577+fengp@users.noreply.github.com>
2025-01-03 16:25:20 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,40 @@
-import streamlit as st
+# Modified from Johannes Rieke's example code
 
-st.title("🎈 My new Streamlit app test")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+from snowflake.snowpark import Session
+
+st.title('❄️ How to connect Streamlit to a Snowflake database')
+
+# Establish Snowflake session
+@st.cache_resource
+def create_session():
+    return Session.builder.configs(st.secrets.snowflake).create()
+
+session = create_session()
+st.success("Connected to Snowflake!")
+
+# Load data table
+@st.cache_data
+def load_data(table_name):
+    ## Read in data table
+    st.write(f"Here's some example data from `{table_name}`:")
+    table = session.table(table_name)
+    
+    ## Do some computation on it
+    table = table.limit(100)
+    
+    ## Collect the results. This will run the query and download the data
+    table = table.collect()
+    return table
+
+# Select and display data table
+table_name = "PETS.PUBLIC.MYTABLE"
+
+## Display data table
+with st.expander("See Table"):
+    df = load_data(table_name)
+    st.dataframe(df)
+
+## Writing out data
+for row in df:
+    st.write(f"{row[0]} has a :{row[1]}:")
